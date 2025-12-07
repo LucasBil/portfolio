3a4edb958ef6f1553667efafea212216e0238460
LucasBil <Lucas.Billy@etu.univ-savoie.fr>
2025-12-07 22:24:16 +0100

    fix i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,17 @@
 import { createI18n } from 'vue-i18n'
 import fr from './fr.json'
 import en from './en.json'
+import ch from './ch.json'
+
+const browserLocale = (navigator.language || 'en').split('-')[0]
 
 export default createI18n({
   legacy: false,
-  locale: 'fr',
+  locale: browserLocale,
   fallbackLocale: 'en',
   messages: {
     fr,
-    en
+    en,
+    ch
   }
 })
