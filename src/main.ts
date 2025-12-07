5823a4ea87a6def2bb6b8e48403212085f0450a6
LucasBil <Lucas.Billy@etu.univ-savoie.fr>
2025-12-07 12:27:19 +0100

    change js to ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import i18n from './i18n'
+import i18n from './i18n/index'
 import router from './router'
 
 const app = createApp(App)
